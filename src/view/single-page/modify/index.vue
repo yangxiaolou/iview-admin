90f4f0565c5c4ebded5d73ac9ad2d840ed56794d
dtid_01c226faf21e1931 <2898234238@qq.com>
2019-09-29 22:48:24 +0800

    更新页面信息

---
+++ src/view/single-page/modify/index.vue
@@ -0,0 +1,176 @@
+<template>
+  <Form ref="formCustom" :model="formCustom" :rules="ruleCustom" :label-width="80">
+    <FormItem label="原密码" prop="passwd">
+      <Input type="password" v-model="formCustom.passwd"></Input>
+    </FormItem>
+    <FormItem label="新密码" prop="passwdCheck">
+      <Input type="password" v-model="formCustom.passwdCheck"></Input>
+    </FormItem>
+    <FormItem label="头像">
+      <div class="demo-upload-list" v-for="item in uploadList">
+        <template v-if="item.status === 'finished'">
+          <img :src="item.url">
+          <div class="demo-upload-list-cover">
+            <Icon type="ios-trash-outline" @click.native="handleRemove(item)"></Icon>
+          </div>
+        </template>
+        <template v-else>
+          <Progress v-if="item.showProgress" :percent="item.percentage" hide-info></Progress>
+        </template>
+      </div>
+      <Upload v-show="!hadImage"
+        ref="upload"
+        :show-upload-list="false"
+        :on-success="handleSuccess"
+        :format="['jpg','jpeg','png']"
+        :max-size="2048"
+        :on-format-error="handleFormatError"
+        :on-exceeded-size="handleMaxSize"
+        :before-upload="handleBeforeUpload"
+        type="drag"
+        action="/api/file/image/upload"
+        style="display: inline-block;width:58px;">
+        <div style="width: 58px;height:58px;line-height: 58px;">
+          <Icon type="ios-camera" size="20"></Icon>
+        </div>
+      </Upload>
+    </FormItem>
+    <FormItem>
+      <Button type="primary" @click="handleSubmit('formCustom')">Submit</Button>
+      <Button @click="handleReset('formCustom')" style="margin-left: 8px">Reset</Button>
+    </FormItem>
+  </Form>
+</template>
+<script>
+import { modify } from '@/api/user'
+export default {
+  data () {
+    const validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入原密码'))
+      } else {
+        callback()
+      }
+    }
+    const validatePassCheck = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入新密码'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      formCustom: {
+        passwd: '',
+        passwdCheck: '',
+        avator: ''
+      },
+      ruleCustom: {
+        passwd: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        passwdCheck: [
+          { validator: validatePassCheck, trigger: 'blur' }
+        ]
+      },
+      uploadList: [],
+      hadImage: false
+    }
+  },
+  methods: {
+    handleSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          modify(this.formCustom).then((res) => {
+            if (res.data.success) {
+              this.$Message.success('提交成功')
+              this.$router.push('/')
+            } else {
+              this.$Message.error(res.data.message)
+            }
+          })
+        } else {
+          this.$Message.error('Fail!')
+        }
+      })
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields()
+    },
+    handleRemove (file) {
+      const fileList = this.$refs.upload.fileList
+      this.$refs.upload.fileList.splice(fileList.indexOf(file), 1)
+      this.hadImage = false
+      this.formCustom.avator = ''
+    },
+    handleSuccess (res, file) {
+      file.url = res.url
+      file.name = res.currentFileName
+      this.formCustom.avator = res.url
+    },
+    handleFormatError (file) {
+      this.$Notice.warning({
+        title: 'The file format is incorrect',
+        desc: 'File format of ' + file.name + ' is incorrect, please select jpg or png.'
+      })
+    },
+    handleMaxSize (file) {
+      this.$Notice.warning({
+        title: 'Exceeding file size limit',
+        desc: 'File  ' + file.name + ' is too large, no more than 2M.'
+      })
+    },
+    handleBeforeUpload () {
+      const check = this.uploadList.length < 1
+      if (!check) {
+        this.$Notice.warning({
+          title: 'Up to five pictures can be uploaded.'
+        })
+      }
+      this.hadImage = true
+      return check
+    }
+  },
+  mounted () {
+    this.uploadList = this.$refs.upload.fileList
+  }
+}
+</script>
+<style>
+  .demo-upload-list{
+    display: inline-block;
+    width: 60px;
+    height: 60px;
+    text-align: center;
+    line-height: 60px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    overflow: hidden;
+    background: #fff;
+    position: relative;
+    box-shadow: 0 1px 1px rgba(0,0,0,.2);
+    margin-right: 4px;
+  }
+  .demo-upload-list img{
+    width: 100%;
+    height: 100%;
+  }
+  .demo-upload-list-cover{
+    display: none;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0,0,0,.6);
+  }
+  .demo-upload-list:hover .demo-upload-list-cover{
+    display: block;
+  }
+  .demo-upload-list-cover i{
+    color: #fff;
+    font-size: 20px;
+    cursor: pointer;
+    margin: 0 2px;
+  }
+</style>
